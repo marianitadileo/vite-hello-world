3071c7eaceae4054f47624fb2b5d73b45c3749a6
marianitadileo <mdileo36@gmail.com>
2023-04-17 01:40:46 +0200

    add title and jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        name: "AppHero"
+    }
+</script>
+
+<template>
+    <div class="jumbo">
+        <img src="../assets/jumbotron.jpg" alt="">
+    </div>
+</template>
+
+<style>
+    .jumbo {
+        width: 50%;
+    }
+    img {
+        width: 100%;
+        height: 300px;
+    }
+</style>
